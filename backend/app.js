6c39439501135b26bffeaf97f94007b479905f35
eyueldegefa <you@example.com>
2025-09-25 09:54:18 +0300

    Add Employee Part

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,25 @@
 const express = require('express');
 // import dotenv to read .env file
 require('dotenv').config();
+// import sanitize module to prevent SQL injection attacks
+const sanitize = require('sanitize');
+// import cors module to allow cross-origin requests
+const cors = require('cors');
+// add corsOptions to allow requests from the frontend
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    optionsSuccessStatus: 200 
+};
 // import the router
 const router = require('./routes');
 // create an express application
 const app = express();
+// use json middleware to parse json requests
+app.use(express.json());
+// use cors middleware
+app.use(cors(corsOptions));
+// add sanitize as middleware
+app.use(sanitize.middleware);
 // add routes to the application as middleware
 app.use(router);
 // create a variable to hold our port number
