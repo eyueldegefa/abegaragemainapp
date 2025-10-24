a0d4004d72a78c8b311d802c6b5e898167911323
eyueldegefa <you@example.com>
2025-10-24 23:45:01 +0300

    Authorization finished

--- backend/routes/employee.route.js
+++ backend/routes/employee.route.js
@@ -2,9 +2,11 @@
 const express = require('express');
 // Call the router method from express to create the router 
 const router = express.Router();
+// Import the auth middleware
+const authMiddleware = require('../middlewares/auth.middleware');
 // Import the employee controller
 const employeeController = require('../controllers/employee.controller');
 // Create a route to handle the add employee request on post
-router.post("/api/employee", employeeController.createEmployee);
+router.post("/api/employee",[authMiddleware.verifyToken, authMiddleware.isAdmin] , employeeController.createEmployee);
 // Export the router
 module.exports = router;
